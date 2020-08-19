92f193a050ecd47bc98685b097013d694b13ac8b
psb-david-petty <psb-david-petty@users.noreply.github.com>
2020-08-19 17:45:17 -0400

    Added shuffle for background-color. Added colorizing based on color classes.

--- rg-quotations.js
+++ rg-quotations.js
@@ -1,16 +1,34 @@
-/*
+/* rg-quotations is a webpage system based on a classroom poster 
+ * by Ria Galanos consisting of quotations in computing.
+ */
+const color_map = {
+    red: '#c33', 
+    green: '#393',
+    blue: '#06c', 
+    purple: '#63c', 
+    orange: '#f90', 
+    cyan: '#0cc', 
+};
+
+/* Format takes ...
  */
 function format(size, spacing) {
     /* Set body line-height to spacing ratio. */
     if (document.querySelector('body'))
         document.querySelector('body').style.lineHeight = `${spacing}`;
+
     /* TODO: finish comment
      */
-    let article = 'article', quote = '.quote', author = '.author', child;
-    for (let node of document.querySelectorAll(article)) {
-        let count = 0, log = '';
+    let colors = Object.values(color_map);
+    let article = 'article', quote = '.quote', author = '.author', child, log;
+    for (const [index, node] of document.querySelectorAll(article).entries()) {
+        /* Rearrange color array after all are used. */
+        if (index % colors.length == 0) {
+            colors = shuffle(colors);
+        }
 
         /* Find number of lines of author. */
+        let count = 0;
         child = node.querySelector(author);
         if (child) {
             let lines = child.innerHTML.split(/\r?\n/);
@@ -22,7 +40,7 @@ function format(size, spacing) {
             }
             else
                 child.style.display = 'none';
-            log += `AUTHOR: "${child.innerHTML}" lines = ${count}`;
+            log = `AUTHOR: "${child.innerHTML}" lines = ${count}`;
         }
 
         /* Find number of characters of quote and use it to set font-size. */
@@ -45,32 +63,65 @@ function format(size, spacing) {
                 + `RATIO: ${ratio} "${fontSize}"`);
             child.style.fontSize = fontSize;
 
-            /* Set random colors from list. */
-            const colors = [ '#0cc', '#63c', '#f90', '#c33', '#06c', '#393', ];
-            let color = colors[Math.floor(Math.random() * colors.length)];
+            /* Set random colors from shuffled list. */
+            let color = colors[index % colors.length];
             node.style.color = 'white';
             node.style.backgroundColor = color;
         }
     }
+    /* Set specific colors with colorize. */
+    colorize(color_map);
+
     /* Set body display to make visible. */
     if (document.querySelector('body'))
         document.querySelector('body').style.visibility = `visible`;
 
-    /* Secial case: make the last element red. */
-    if (document.querySelector('#last'))
-        document.querySelector('#last').style.backgroundColor = `#c33`;
+    return false;
+}
 
+/* Colorize particular elements based on classes in keys of the colors object.
+ */
+function colorize(colors) {
+    for (const color in colors) {
+        // Select all nodes matching color class and set background-color.
+        for (const node of document.querySelectorAll(`.${color}`)) {
+            node.style.backgroundColor = colors[color];
+        }
+    }
     return false;
 }
 
 // https://stackoverflow.com/questions/31305071/measuring-text-width-height-without-rendering
 function get_text_size(txt, font) {
     let element = document.createElement('canvas');
-    let context = element.getContext("2d");
+    let context = element.getContext('2d');
     context.font = font;
     let tsize = {
         'width': context.measureText(txt).width, 
-        'height':parseInt(context.font)
+        'height': parseInt(context.font)
     };
     return tsize;
+}
+
+/* https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) 
+ * Shuffle. See https://github.com/coolaj86/knuth-shuffle.
+ */
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
